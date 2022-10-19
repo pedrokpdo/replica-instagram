e14658dea5e220cae456a40625d052d25377c2bd
pedrokpdo <pedromotoehm@gmail.com>
2022-10-19 14:51:58 -0300

    Created a new Expo app

---
+++ src/components/funcionais/ContainerComponent/index.tsx
@@ -0,0 +1,22 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { ScrollView } from 'react-native'
+import { Space } from "../SpaceComponent";
+
+interface Props {
+    children?: any;
+    back?: any;
+}
+
+export const ContainerComponent = ({ children, back }: Props) => {
+    return (
+        <ScrollView style={{ flex: 1, backgroundColor: 'rgb(19, 18, 18)' }}>
+            {back ? (
+                <Space top={1}>
+                    <Ionicons name="arrow-back" size={24} color={'white'} />
+                </Space>
+            ) : null}
+            {children}
+        </ScrollView>
+    )
+}
