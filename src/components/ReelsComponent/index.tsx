546b7cf58ece1af3c11f549207fa1bbceaf4bbb3
pedrokpdo <pedromotoehm@gmail.com>
2022-10-20 14:24:03 -0300

    ReelsScreen

---
+++ src/components/ReelsComponent/index.tsx
@@ -0,0 +1,40 @@
+import { AntDesign, Entypo, Ionicons, SimpleLineIcons } from "@expo/vector-icons";
+import React from "react";
+import { Dimensions, ImageBackground, View } from "react-native";
+import { COLORS } from "../../colors";
+import { AvatarComponent } from "../funcionais/AvatarComponent";
+import { Row } from "../funcionais/RowComponent";
+import { Space } from "../funcionais/SpaceComponent";
+import { Text } from "../funcionais/TextComponent";
+import { Title } from "../funcionais/TitleComponent";
+
+const { width, height } = Dimensions.get('window')
+
+export const ReelsComponent = ({ photo, name, describe, message, music }) => {
+    return (
+        <ImageBackground source={photo} style={{ width: width, height: height - 70 }}>
+            <Space top={2}>
+                <Row style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Title style={{ color: COLORS.text_inverted, fontSize: 24 }}>Reels</Title>
+                    <AntDesign name="camerao" size={24} color={COLORS.text_inverted} />
+                </Row>
+                <View style={{ alignSelf: 'flex-end', marginTop: 100, alignItems: 'center' }}>
+                    <AntDesign style={{ marginTop: 24 }} name={'like1'} size={32} color={COLORS.text_inverted} />
+                    <AntDesign style={{ marginTop: 24 }} name={'dislike1'} size={32} color={COLORS.text_inverted} />
+                    <AntDesign style={{ marginTop: 24 }} name={'message1'} size={32} color={COLORS.text_inverted} />
+                    <Entypo style={{ marginTop: 24 }} name={'direction'} size={32} color={COLORS.text_inverted} />
+                    <SimpleLineIcons style={{ marginTop: 24 }} name={'options-vertical'} size={24} color={COLORS.text_inverted} />
+                </View>
+            </Space>
+            <Space>
+                <Row>
+                    <AvatarComponent photo={photo} />
+                    <Title style={{ color: COLORS.text_inverted }}>{name}</Title>
+                </Row>
+                <Text style={{ color: COLORS.text_inverted }}>{describe}</Text>
+                <Title top={.5} style={{ color: COLORS.text_inverted }}>{message}</Title>
+                <Title top={.5} style={{ color: COLORS.text_inverted }}>{music}</Title>
+            </Space>
+        </ImageBackground>
+    )
+}
--- src/screens/ReelsScreen/index.tsx
+++ src/screens/ReelsScreen/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { View } from "react-native";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
+import { ReelsComponent } from "../../components/ReelsComponent";
+//@ts-ignore
+import perfil2 from '../../assets/perfil2.jpg'
+//@ts-ignore
+import perfil3 from '../../assets/perfil3.jpg'
+//@ts-ignore
+import perfil4 from '../../assets/perfil4.jpg'
+//@ts-ignore
+import perfil5 from '../../assets/perfil5.webp'
+//@ts-ignore
+import perfil6 from '../../assets/perfil6.webp'
 
 export const ReelsScreen = () => {
     return (
         <ContainerComponent>
-            
+            <ReelsComponent name={'Fatos Desconhecidos'} photo={perfil2} describe='Remix com cretinozuero' message={'Massagem é tratamento, terapia e cura'} music='Dj Dark, Mentol . Mood (Radio Edit)'/>
+            <ReelsComponent name={'Fatos Desconhecidos'} photo={perfil2} describe='Remix com cretinozuero' message={'Massagem é tratamento, terapia e cura'} music='Dj Dark, Mentol . Mood (Radio Edit)'/>
+            <ReelsComponent name={'Fatos Desconhecidos'} photo={perfil2} describe='Remix com cretinozuero' message={'Massagem é tratamento, terapia e cura'} music='Dj Dark, Mentol . Mood (Radio Edit)'/>
         </ContainerComponent>
     )
 }
